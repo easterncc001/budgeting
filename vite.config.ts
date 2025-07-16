8506e448b6d9a0b72720c6973c02c65027d46771
easterncc001 <prestonlee847@gmail.com>
2025-07-16 16:03:49 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // explicitly set root to current directory
+  publicDir: 'public', // explicitly set public directory
   plugins: [react()],
 }); 
